4452bea3bb0e1c770cbb3d80577a9911ced6eb3a
JangJanPing <810230010@qq.com>
2017-06-30 09:50:59 +0800

    crawl

--- src/main/java/com/wuliangit/elevator/DoMain.java
+++ src/main/java/com/wuliangit/elevator/DoMain.java
@@ -142,7 +142,7 @@ public class DoMain {
         //舟山市定海区公共资源交易中心(招标)
         //Spider.create(new DhggzyBefore1()).addRequest(DhggzyBefore1.getRequest()).thread(2).run();
         //舟山市定海区公共资源交易中心(中标)
-        Spider.create(new DhggzyAfter1()).addRequest(DhggzyAfter1.getRequest()).thread(2).run();
+        //Spider.create(new DhggzyAfter1()).addRequest(DhggzyAfter1.getRequest()).thread(2).run();
     }
 
 }
